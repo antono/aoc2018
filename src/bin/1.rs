7333f4f08082320f19a74a769627cb60b69944b0
Anton Vasiljev <antono.vasiljev@gmail.com>
2018-12-10 01:41:42 +0300

    Puzzle #1 part 2

--- src/bin/1.rs
+++ src/bin/1.rs
@@ -24,14 +24,45 @@
 //
 // Starting with a frequency of zero, what is the resulting frequency after all
 // of the changes in frequency have been applied?
+//
+// --- Part Two ---
+//
+// You notice that the device repeats the same frequency change list over and
+// over. To calibrate the device, you need to find the first frequency it
+// reaches twice.
+//
+// For example, using the same list of changes above, the device would loop as
+// follows:
+//
+// Current frequency  0, change of +1; resulting frequency  1.
+// Current frequency  1, change of -2; resulting frequency -1.
+// Current frequency -1, change of +3; resulting frequency  2.
+// Current frequency  2, change of +1; resulting frequency  3.
+// (At this point, the device continues from the start of the list.)
+// Current frequency  3, change of +1; resulting frequency  4.
+// Current frequency  4, change of -2; resulting frequency  2, which has already been seen.
+//
+// In this example, the first frequency reached twice is 2. Note that your
+// device might need to repeat its list of frequency changes many times before a
+// duplicate frequency is found, and that duplicates might be found while in the
+// middle of processing the list.
+//
+// Here are other examples:
+//
+// +1, -1 first reaches 0 twice.
+// +3, +3, +4, -2, -4 first reaches 10 twice.
+// -6, +3, +8, +5, -6 first reaches 5 twice.
+// +7, +7, -2, -7, -4 first reaches 14 twice.
 
 use std::fs::File;
 use std::io::prelude::*;
+use std::collections::HashSet;
 
 // TODO: Share between puzzles...
 fn read_puzzle_input(number: u8) -> String {
     let mut input =
         File::open(format!("./inputs/{}.txt", number)).expect("Puzzle input not found...");
+
     let mut text = String::new();
 
     input
@@ -41,7 +72,7 @@ fn read_puzzle_input(number: u8) -> String {
     return text;
 }
 
-fn main() {
+fn part_one() {
     let input = read_puzzle_input(1);
     let mut result: i32 = 0;
 
@@ -50,5 +81,28 @@ fn main() {
         result += num;
     }
 
-    println!("{}", result);
+    println!("Sum of freq adjustments: {}", result);
+}
+
+fn part_two() {
+    let input = read_puzzle_input(1);
+    let mut result: i32 = 0;
+    let mut seen_freqs: HashSet<i32> = HashSet::new();
+
+    seen_freqs.insert(result);
+
+    for line in input.lines().cycle() {
+        let num = line.parse::<i32>().unwrap();
+        result += num;
+
+        if !seen_freqs.insert(result) {
+            println!("Repeating frequency: {}", result);
+            break;
+        }
+    }
+}
+
+fn main() {
+    part_one();
+    part_two();
 }
