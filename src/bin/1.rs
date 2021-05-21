31f64cb9700c44c4e94f4f8f315012649b2494ce
Anton Vasiljev <antono.vasiljev@onthespotdev.com>
2021-05-21 10:42:58 +0300

    Moved comments to proper locations

--- src/bin/1.rs
+++ src/bin/1.rs
@@ -25,6 +25,24 @@
 // Starting with a frequency of zero, what is the resulting frequency after all
 // of the changes in frequency have been applied?
 //
+
+extern crate utils;
+
+use std::collections::HashSet;
+
+fn part_one() {
+    let input = utils::read_puzzle_input(1);
+    let mut result: i32 = 0;
+
+    for line in input.lines() {
+        let num: i32 = line.parse().unwrap_or_default();
+        result += num;
+    }
+
+    println!("--- Part 1 ---");
+    println!("Sum of freq adjustments: {}", result);
+}
+
 // --- Part Two ---
 //
 // You notice that the device repeats the same frequency change list over and
@@ -53,24 +71,7 @@
 // +3, +3, +4, -2, -4 first reaches 10 twice.
 // -6, +3, +8, +5, -6 first reaches 5 twice.
 // +7, +7, -2, -7, -4 first reaches 14 twice.
-
-extern crate utils;
-
-use std::collections::HashSet;
-
-fn part_one() {
-    let input = utils::read_puzzle_input(1);
-    let mut result: i32 = 0;
-
-    for line in input.lines() {
-        let num: i32 = line.parse().unwrap_or_default();
-        result += num;
-    }
-
-    println!("--- Part 1 ---");
-    println!("Sum of freq adjustments: {}", result);
-}
-
+//
 fn part_two() {
     let input = utils::read_puzzle_input(1);
     let mut result: i32 = 0;
