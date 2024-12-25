025200eaf3b1e67b968f32b162c07124864772a9
Anton Vasiljev <antono.vasiljev@gmail.com>
2024-12-25 03:55:55 +0300

    Puzzle 6.2

--- src/bin/6.rs
+++ src/bin/6.rs
@@ -92,8 +92,15 @@ impl Point {
         }
     }
 
-    pub fn rectilinear_distance(&self, b: &Point) -> u32 {
-        ((self.x as i32 - b.x as i32).abs() + (self.y as i32 - b.y as i32).abs()) as u32
+    pub fn rectilinear_distance(&self, other: &Point) -> u32 {
+        ((self.x as i32 - other.x as i32).abs() + (self.y as i32 - other.y as i32).abs()) as u32
+    }
+
+    pub fn sum_distances(&self, points: Vec<Point>) -> u32 {
+        points
+            .iter()
+            .map(|point| self.rectilinear_distance(&point))
+            .sum()
     }
 
     //
@@ -125,20 +132,25 @@ struct World {
     points: Vec<Point>,
     map: Vec<Vec<String>>,
     proximity_map: Vec<Vec<String>>,
+    closest_map: Vec<Vec<String>>,
 }
 
 impl World {
     fn new(points: Vec<Point>, width: usize, height: usize) -> World {
         let map = vec![vec![String::from("."); width]; height];
         let proximity_map = vec![vec![String::from("."); width]; height]; // 10x10
+        let closest_map = vec![vec![String::from("."); width]; height]; // 10x10
+
         let mut world = World {
             points,
             map,
             proximity_map,
+            closest_map,
         };
 
         world.build_map();
         world.build_proximity_map();
+        world.build_closest_map(10_000); // 10000 comes from requirements
 
         world
     }
@@ -170,18 +182,41 @@ impl World {
         }
     }
 
+    fn build_closest_map(&mut self, max_allowed_distance: u32) {
+        for (i, row) in self.map.iter().enumerate() {
+            for (j, cell) in row.iter().enumerate() {
+                let point = Point::new("?", j as i32, i as i32);
+                let total_distance = point.sum_distances(self.points.clone());
+
+                if total_distance < max_allowed_distance {
+                    self.closest_map[i][j] = String::from("#");
+                } else {
+                    self.closest_map[i][j] = String::from(cell);
+                }
+            }
+        }
+    }
+
+    fn closest_island_size(&self) -> u32 {
+        self.closest_map
+            .iter()
+            .flat_map(|vec| vec.iter())
+            .map(|cell| if cell == "#" { 1 } else { 0 })
+            .sum()
+    }
+
     // returns number and point for biggest island
     fn find_biggest_island(&self) -> (u32, String) {
-        let mut not_islands = HashSet::new();
+        let mut unlimited_islands = HashSet::new();
         let mut counts = HashMap::new();
 
-        not_islands.insert(String::from("."));
+        unlimited_islands.insert(String::from("."));
 
         for (i, row) in self.proximity_map.iter().enumerate() {
             for (j, col) in row.iter().enumerate() {
                 // border points belongs to unlimited islands
                 if i == 0 || i == 9 || j == 0 || j == 9 {
-                    not_islands.insert(col.to_ascii_lowercase());
+                    unlimited_islands.insert(col.to_ascii_lowercase());
                     continue;
                 }
 
@@ -192,12 +227,12 @@ impl World {
             }
         }
 
-        // let max = counts.iter().filter(|(k,v)| !not_islands.contains(*k)).max_by_key(|(k,v)| v).unwrap();
+        // let max = counts.iter().filter(|(k,v)| !unlimited_islands.contains(*k)).max_by_key(|(k,v)| v).unwrap();
 
         // println!("{:?}", max)
 
         let keys: HashSet<String> = counts.keys().cloned().collect();
-        let real_islands = keys.difference(&not_islands);
+        let real_islands = keys.difference(&unlimited_islands);
 
         let mut max_island = String::from(".");
         let mut max_island_size = 0;
@@ -209,24 +244,73 @@ impl World {
             }
         }
 
-
-        // println!("{:#?}", not_islands);
+        // println!("{:#?}", unlimited_islands);
         // println!("{:#?}", counts);
-        // println!("{:#?}", keys.difference(&not_islands));
+        // println!("{:#?}", keys.difference(&unlimited_islands));
 
         (max_island_size, max_island)
     }
 }
 
-fn part_one(points: Vec<Point>) {
-    let width = points.iter().max_by_key(|p| p.x).unwrap().x as usize;
-    let height = points.iter().max_by_key(|p| p.y).unwrap().y as usize;
-
+fn part_one(points: Vec<Point>, width: usize, height: usize) {
     let world = World::new(points, width + 1, height + 1);
     let (biggest_island_size, _) = world.find_biggest_island();
 
     println!("--- Part 1 ---");
     println!("Biggest island size: {:?}", biggest_island_size);
+}
+
+// --- Part Two ---
+//
+// On the other hand, if the coordinates are safe, maybe the best you can do is
+// try to find a region near as many coordinates as possible.
+//
+// For example, suppose you want the sum of the Manhattan distance to all of the
+// coordinates to be less than 32. For each location, add up the distances to
+// all of the given coordinates; if the total of those distances is less than
+// 32, that location is within the desired region. Using the same coordinates as
+// above, the resulting region looks like this:
+//
+// ..........
+// .A........
+// ..........
+// ...###..C.
+// ..#D###...
+// ..###E#...
+// .B.###....
+// ..........
+// ..........
+// ........F.
+//
+// In particular, consider the highlighted location 4,3 located at the top
+// middle of the region. Its calculation is as follows, where abs() is the
+// absolute value function:
+//
+//     Distance to coordinate A: abs(4-1) + abs(3-1) =  5
+//     Distance to coordinate B: abs(4-1) + abs(3-6) =  6
+//     Distance to coordinate C: abs(4-8) + abs(3-3) =  4
+//     Distance to coordinate D: abs(4-3) + abs(3-4) =  2
+//     Distance to coordinate E: abs(4-5) + abs(3-5) =  3
+//     Distance to coordinate F: abs(4-8) + abs(3-9) = 10
+//     Total distance: 5 + 6 + 4 + 2 + 3 + 10 = 30
+//
+// Because the total distance to all coordinates (30) is less than 32, the
+// location is within the region.
+//
+// This region, which also includes coordinates D and E, has a total size of 16.
+//
+// Your actual region will need to be much larger than this example, though,
+// instead including all locations with a total distance of less than 10000.
+//
+// What is the size of the region containing all locations which have a total
+// distance to all given coordinates of less than 10000?
+
+fn part_two(points: Vec<Point>, width: usize, height: usize) {
+    let world = World::new(points, width + 1, height + 1);
+    let size = world.closest_island_size();
+
+    println!("--- Part 2 ---");
+    println!("Closest island size: {:?}", size);
 }
 
 fn main() {
@@ -244,7 +328,11 @@ fn main() {
         points.push(point.clone());
     }
 
-    part_one(points);
+    let width = points.iter().max_by_key(|p| p.x).unwrap().x as usize;
+    let height = points.iter().max_by_key(|p| p.y).unwrap().y as usize;
+
+    part_one(points.clone(), width, height);
+    part_two(points.clone(), width, height);
 }
 
 #[cfg(test)]
@@ -311,17 +399,17 @@ mod tests {
 
         let expected_proximity_map_str = indoc![
             "
-            aaaaa.cccc
-            aAaaa.cccc
-            aaaddecccc
-            aadddeccCc
-            ..dDdeeccc
-            bb.deEeecc
-            bBb.eeee..
-            bbb.eeefff
-            bbb.eeffff
-            bbb.ffffFf
-        "
+                aaaaa.cccc
+                aAaaa.cccc
+                aaaddecccc
+                aadddeccCc
+                ..dDdeeccc
+                bb.deEeecc
+                bBb.eeee..
+                bbb.eeefff
+                bbb.eeffff
+                bbb.ffffFf
+            "
         ];
 
         assert_eq!(proximity_map_str, expected_proximity_map_str);
@@ -407,38 +495,97 @@ mod tests {
 
         let expected_map_str = indoc![
             "
-            ..........
-            .A........
-            ..........
-            ........C.
-            ...D......
-            .....E....
-            .B........
-            ..........
-            ..........
-            ........F.
-        "
+                ..........
+                .A........
+                ..........
+                ........C.
+                ...D......
+                .....E....
+                .B........
+                ..........
+                ..........
+                ........F.
+            "
         ];
 
         let expected_proximity_map_str = indoc![
             "
-            aaaaa.cccc
-            aAaaa.cccc
-            aaaddecccc
-            aadddeccCc
-            ..dDdeeccc
-            bb.deEeecc
-            bBb.eeee..
-            bbb.eeefff
-            bbb.eeffff
-            bbb.ffffFf
-        "
+                aaaaa.cccc
+                aAaaa.cccc
+                aaaddecccc
+                aadddeccCc
+                ..dDdeeccc
+                bb.deEeecc
+                bBb.eeee..
+                bbb.eeefff
+                bbb.eeffff
+                bbb.ffffFf
+            "
         ];
 
         assert_eq!(expected_map_str, map_str);
         assert_eq!(expected_proximity_map_str, proximity_map_str);
+    }
 
-        // println!("{}", proximity_map_str);
-        // println!("{}", expected_proximity_map_str);
+    #[test]
+    fn test_sum_distances_to_all_points() {
+        let test_points = vec![
+            Point::new("A", 1, 1),
+            Point::new("B", 1, 6),
+            Point::new("C", 8, 3),
+            Point::new("D", 3, 4),
+            Point::new("E", 5, 5),
+            Point::new("F", 8, 9),
+        ];
+
+        let some_point = Point::new("#", 4, 3);
+
+        let sum = some_point.sum_distances(test_points);
+
+        assert_eq!(sum, 30)
+    }
+
+    #[test]
+    fn test_build_closest_map() {
+        let test_points = vec![
+            Point::new("A", 1, 1),
+            Point::new("B", 1, 6),
+            Point::new("C", 8, 3),
+            Point::new("D", 3, 4),
+            Point::new("E", 5, 5),
+            Point::new("F", 8, 9),
+        ];
+        let mut world = World::new(test_points, 10, 10);
+
+        world.build_closest_map(32);
+
+        let mut closest_map_str = String::new();
+
+        for row in world.closest_map.iter() {
+            for cell in row.iter() {
+                closest_map_str.push_str(cell);
+            }
+            closest_map_str.push_str(&"\n");
+        }
+
+        let expected_closest_map_str = indoc![
+            "
+                ..........
+                .A........
+                ..........
+                ...###..C.
+                ..#D###...
+                ..###E#...
+                .B.###....
+                ..........
+                ..........
+                ........F.
+            "
+        ];
+
+        assert_eq!(expected_closest_map_str, expected_closest_map_str);
+
+        let closest_island_size = world.closest_island_size();
+        assert_eq!(closest_island_size, 16);
     }
 }
