352c7dd254d3cf7a0e7d3e5fba324c1efab9085e
Anton Vasiljev <antono.vasiljev@gmail.com>
2025-01-13 23:04:21 +0300

    Updated devenv.sh

--- devenv.nix
+++ devenv.nix
@@ -3,9 +3,13 @@
 {
   # https://devenv.sh/basics/
   env.RUST_BACKTRACE = 1;
+  env.AOC_DAY = 7;
 
   # https://devenv.sh/packages/
-  packages = [ pkgs.git ];
+  packages = with pkgs; [
+    git
+    tokei
+  ];
 
   # https://devenv.sh/languages/
   languages.rust.enable = true;
@@ -17,12 +21,23 @@
   # services.postgres.enable = true;
 
   # https://devenv.sh/scripts/
-  scripts.hello.exec = ''
-    echo hello from $GREET
+  scripts.test.exec = ''
+    echo "Running tests"
+    for i in $(seq 1 $AOC_DAY); do
+      cargo test --bin $i
+    done
+  '';
+
+  scripts.run.exec = ''
+    echo "Running tests"
+    for i in $(seq 1 $AOC_DAY); do
+      cargo run --bin $i
+    done
   '';
 
   enterShell = ''
     git --version
+    tokei
   '';
 
   # https://devenv.sh/tasks/
@@ -34,7 +49,7 @@
   # https://devenv.sh/tests/
   enterTest = ''
     echo "Running tests"
-    for i in $(seq 1 7); do
+    for i in $(seq 1 $AOC_DAY); do
       cargo test --bin $i
     done
   '';
